94ad27fbf564ced02500688141a4805fb384f873
Elia Cereda <eliacereda@gmail.com>
2012-06-30 15:45:10 +0200

    Initial Commit

---
+++ TagLib-ObjC/TagLib_ObjC.h
@@ -0,0 +1,13 @@
+//
+//  TagLib_ObjC.h
+//  TagLib-ObjC
+//
+//  Created by Me on 30/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TagLib_ObjC : NSObject
+
+@end
---
+++ TagLib-ObjC/TagLib_ObjC.m
@@ -0,0 +1,13 @@
+//
+//  TagLib_ObjC.m
+//  TagLib-ObjC
+//
+//  Created by Me on 30/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "TagLib_ObjC.h"
+
+@implementation TagLib_ObjC
+
+@end
