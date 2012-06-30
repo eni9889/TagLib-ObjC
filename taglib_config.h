e252fb5f50f841009deff26af7212778b56f3687
Elia Cereda <eliacereda@gmail.com>
2012-06-30 16:46:37 +0200

    Added libtaglib.a target

---
+++ taglib_config.h
@@ -0,0 +1,15 @@
+//
+//  taglib_config.h
+//  TagLib-ObjC
+//
+//  Created by Me on 30/06/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef TagLib_ObjC_taglib_config_h
+#define TagLib_ObjC_taglib_config_h
+
+#define   TAGLIB_WITH_ASF 1
+#define   TAGLIB_WITH_MP4 1
+
+#endif
